1555d56f539353464e69c7ac7f518199c50e525c
VincentKiwi <vincent.kiwanuka@elev.ga.ntig.se>
2024-12-10 01:12:01 +0100

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿string[] names = {"leksak1", "leksak2", "leksak3", "leksak4", "leksak5"};
+string[] persons = {"a1", "a2", "a3", "a4", "a5"};
+int[] number = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10,};
+int index = 0;
+
+string[] cities = {};
+
+foreach (string name in names)
+{
+    Console.WriteLine($"{name} fjsifjds persons {persons [index]} sadasfafs {number [index]} ");
+
+    index++;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
